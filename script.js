f20605f5aba85c8a59b45965eef2e61a017bf530
bing yang <bingyang9258@gmail.com>
2022-09-07 15:39:02 -0400

    add loading feature

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 function getNewQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
     if (!quote.author) {
@@ -23,15 +34,18 @@ function getNewQuote() {
     }
     
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // get quotes from api
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
         getNewQuote();
+        complete();
     } catch (error) {
         // catch error here
 
