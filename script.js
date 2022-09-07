49f335cca96545a956fd150ce23647d3a93fd66b
bing yang <bingyang9258@gmail.com>
2022-09-07 14:11:00 -0400

    create and style initial page

---
+++ script.js
@@ -0,0 +1,49 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+let apiQuotes = [];
+
+function getNewQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    
+    quoteText.textContent = quote.text;
+}
+
+// get quotes from api
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        getNewQuote();
+    } catch (error) {
+        // catch error here
+
+    }
+}
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', getNewQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+getQuotes();
